58f0e6ab9176297e974aabe4f6ef49a0258414ad
pharaoh1205 <momo1205.968@gmail.com>
2026-03-25 12:30:58 +0900

    初回コミット（おみくじアプリ）

---
+++ script.js
@@ -0,0 +1,17 @@
+function getRandomImage(){
+    const number = Math.floor(Math.random() * 7);
+    const imagePath = "./images/omikuji-" + number.toString() + ".png";
+    return imagePath
+}
+
+function playOmikuji(){
+  const timer = setInterval(function(){
+      document.querySelector("#js-result").setAttribute("src", getRandomImage());
+  }, 100);
+
+  setTimeout(function(){
+    clearInterval(timer);
+  }, 3000)
+}
+
+document.querySelector("#js-button").addEventListener("click", playOmikuji);
